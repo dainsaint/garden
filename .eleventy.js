f4ca5b2b1289976d739c76a49c31c8f2f16cbf92
Dain Saint <dain@cipherprime.com>
2020-05-26 15:19:14 -0400

    trying this build doohickey

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,6 @@ module.exports = function(config)
   config.addPassthroughCopy("assets");
 
   config.addFilter('where', function (array, key, value) {
-    console.log( '>>', value );
   return array.filter(item => {
     const keys = key.split('.');
     const reducedKey = keys.reduce((object, key) => {
