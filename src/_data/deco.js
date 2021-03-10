f0743d2ea5f282ecba8f88d90a5973f4d87eed31
Dain Saint <dain@cipherprime.com>
2021-03-09 21:57:12 -0500

    auto-gen deco

--- src/_data/deco.js
+++ src/_data/deco.js
@@ -4,5 +4,11 @@ const path = require('path');
 module.exports = async () => {
   let filePath = path.join(__dirname, '..', 'assets','img','deco' );
   let files = fs.readdirSync( filePath );
-  return files.filter( x => x.indexOf(".png") >= 0 );
+  const result = {
+    deco: files.filter( x => x.indexOf(".png") >= 0 ).map( x => x.replace('.png', '') )
+  };
+
+  console.log( result );
+
+  return result
 }
